436888eeefcd43e54b9b9a28d6888a1b3769f7df
ryan <ryancruse1807@gmail.com>
2025-11-12 15:54:40 +0000

    Project Setup - Tailwind & Fonts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Online-Portfolio
+
+### Personal online portfolio built with React. 
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl">Test</h1>
+      <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem unde perspiciatis, consequatur iusto officiis repellendus et incidunt? Aut, ipsa earum.</p>
+    </div>
+  )
+}
+
+export default App
