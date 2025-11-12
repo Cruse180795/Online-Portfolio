31d3d5d7bdb4df2a3980247c690f12150e1932ea
ryan <ryancruse1807@gmail.com>
2025-11-12 19:00:27 +0000

    Hero buttons changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,27 @@
 const Hero = () => {
     return ( 
         <section className="px-4 py-5 h-screen space-y-6 text-white" id="hero">
-
             {/** Image Container */}
             <div>
                 <img src="/images/dummy_600x400_ffffff_cccccc.png" alt="Avatar" className="rounded-2xl mx-auto"/>
             </div>
             {/** Text Container */}
             <div className="text-center">
-                <h1 className="text-4xl font-nunito font-semibold tracking-[0.1125em] mb-6">Hey, I'm Ryan</h1>
+                <h1 className="text-4xl font-nunito font-semibold tracking-wider mb-6">Hey, I'm Ryan</h1>
                 <div className="space-y-5 mb-10">
-                    <p className="uppercase font-roboto-mono font-light text-lg ">I am a react and laravel developer based in newcastle upon tyne.</p>
-                    <p className="uppercase font-roboto-mono font-light text-lg ">Building modren, perfomant web apps with style and simplicity.</p>
+                    <p className="uppercase font-roboto-mono font-light text-lg tracking-wide">I am a react and laravel developer based in newcastle upon tyne.</p>
+                    <p className="uppercase font-roboto-mono font-light text-lg tracking-wide">Building modren, perfomant web apps with style and simplicity.</p>
                 </div>
-                <div className=" flex flex-col gap-y-5">
+                <div className=" flex flex-col gap-y-4">
                     <button className=" 
                     cursor-pointer 
-                    w-full
                     border-2 
                     border-white/20 
-                    p-4 
-                    rounded-tl-[20px] 
-                    rounded-br-[20px] 
-                    rounded-tr-[10px] 
-                    rounded-bl-[10px] 
+                    px-4 py-2
+                    rounded-lg
                     font-roboto-mono 
-                    text-lg 
-                    font-light 
-                    tracking-[0.1125em] 
+                    font-semibold
+                    tracking-wide 
                     bg-white/10 
                     backdrop-blur-md 
                     shadow-[0_10px_20px_0_rgba(0,0,0,0.2)]
@@ -39,18 +33,13 @@ const Hero = () => {
                     </button>
                     <button className="
                     cursor-pointer 
-                    w-full
                     border-2 
                     border-white/20 
-                    p-4 
-                    rounded-tl-[20px] 
-                    rounded-br-[20px] 
-                    rounded-tr-[10px] 
-                    rounded-bl-[10px] 
+                    px-4 py-2
+                    rounded-lg 
                     font-roboto-mono 
-                    text-lg 
-                    font-light 
-                    tracking-[0.1125em] 
+                    font-semibold
+                    tracking-wide 
                     shadow-[0_10px_20px_0_rgba(0,0,0,0.2)] 
                     duration-500
                     transition-all
